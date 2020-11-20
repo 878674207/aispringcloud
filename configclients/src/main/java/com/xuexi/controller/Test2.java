4b141aed09c0d8f9ad7406cd8ecfac64feb538a2
878674207 <878674207@qq.com>
2020-11-20 13:52:14 +0800

    1

---
+++ configclients/src/main/java/com/xuexi/controller/Test2.java
@@ -0,0 +1,9 @@
+package com.xuexi.controller;
+
+/**
+ * @Author: wangbaichuan
+ * @Date: 2020/11/20 13:48
+ */
+public class Test2 {
+    private int aa;
+}
---
+++ configclients/src/main/java/com/xuexi/controller/Test3.java
@@ -0,0 +1,8 @@
+package com.xuexi.controller;
+
+/**
+ * @Author: wangbaichuan
+ * @Date: 2020/11/20 13:51
+ */
+public class Test3 {
+}
